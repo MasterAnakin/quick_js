7cc524916497f3a33fd75a65cfe7448ab66e4573
MasterAnakin <mbm4816@gmail.com>
2022-03-07 16:22:17 +0100

    Create url_update.js

---
+++ url_update.js
@@ -0,0 +1,7 @@
+		<script>
+		jQuery(document).ready(function(){
+			    jQuery(".featured-cta-content a").each(function(){
+			        this.href = this.href.replace("https://www.svbleerink.com/contact/", "mailto:Marketing-Communications@svbleerink.com");
+			    });
+			});
+		</script>
